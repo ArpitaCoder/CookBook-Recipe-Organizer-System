b920f99e8cafacb3648abfdab8cce83768dfc83c
Arpita Sharma <arpitasharma93101@gmail.com>
2026-04-13 21:52:24 +0530

    Made diff file for Login and SignIn

---
+++ RecipeToggle.js
@@ -0,0 +1,9 @@
+let indian = document.getElementById("indian");
+let chinese = document.getElementById("chinese");
+let italian = document.getElementById("italian");
+let rice = document.getElementById("rice");
+let beverages = document.getElementById("beverages");
+let desserts = document.getElementById("desserts");
+let Recipee = document.querySelectorAll(".rec");
+let recipes = document.querySelectorAll(".rec");
+
